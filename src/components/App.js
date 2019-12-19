9941b05293e863868fd1a62d331ed7b868195799
CalWal <callumtrwallace@gmail.com>
2019-12-19 18:28:33 +1000

    added action creators index

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import PostList from "./PostList";
 
-class App extends Component {
-  render() {
-    return <div className="ui container">Hello World</div>;
-  }
-}
+const App = () => {
+  return (
+    <div className="ui container">
+      <PostList />
+    </div>
+  );
+};
 
 export default App;
