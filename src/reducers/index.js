5cc021c363fd7cf840aa7c0048c9ca6193f9d492
CalWal <callumtrwallace@gmail.com>
2019-12-23 18:19:38 +1000

    made individual reducers using switch statements

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
 import { combineReducers } from "redux";
+import postReducer from "./postsReducer";
 
-export default combineReducers({ replaceMe: () => "replace me" });
+export default combineReducers({ posts: postReducer });
