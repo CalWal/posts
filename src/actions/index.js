b3b49ded39f0a6f62f76645855b353ceb2ef84d7
CalWal <callumtrwallace@gmail.com>
2019-12-26 18:00:48 +1000

    added lodash to use memoization when fetching users

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonplaceholder from "../apis/jsonplaceholder";
+import _ from "lodash";
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonplaceholder.get("/posts");
@@ -6,8 +7,13 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = id => async dispatch => {
+export const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch);
+};
+
+// added _ for one time memoization
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonplaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
