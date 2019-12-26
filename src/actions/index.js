dda25548c66a864dffdf3bb3b731d82b12142dd7
CalWal <callumtrwallace@gmail.com>
2019-12-26 18:28:16 +1000

    made fetchPostsAndUsers AC with lodash to combine AC's

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 import jsonplaceholder from "../apis/jsonplaceholder";
 import _ from "lodash";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonplaceholder.get("/posts");
 
